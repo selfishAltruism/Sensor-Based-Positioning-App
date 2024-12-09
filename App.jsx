786ab955628197471738ab6c5f034bc491570c95
selfishAltruism <officialkyus@gmail.com>
2024-12-09 14:31:17 +0900

    feat: apply background img & point

--- App.jsx
+++ App.jsx
@@ -1,51 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
-import WifiListScreen from "./src/entities/Test";
-
-import requestBluetoothPermission from "./src/permissions/requestBluetoothPermission";
-
-import Scan from "./src/utils/Scan";
-import ResultModal from "./src/entities/ResultModal";
-
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  PanResponder,
+  Animated,
+} from "react-native";
 
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const scale = useRef(new Animated.Value(1)).current; // 초기 크기 1
 
-  const openModal = () => {
-    setModalVisible(true);
-  };
-
-  const closeModal = () => {
-    setModalVisible(false);
-  };
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        // 드래그 중 좌표 업데이트
+        setPosition({
+          x: gestureState.moveX,
+          y: gestureState.moveY,
+        });
+      },
+    })
+  ).current;
 
   useEffect(() => {
-    requestBluetoothPermission();
-  }, []);
-
-  const onPosition = () => {
-    (async () => {
-      await AsyncStorage.setItem(Date.now() + "-position", JSON.stringify([]));
-    })();
-  };
+    // 확대 및 축소 애니메이션 반복
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scale, {
+          toValue: 1.4, // 확대 비율
+          duration: 500, // 애니메이션 시간 (밀리초)
+          useNativeDriver: true,
+        }),
+        Animated.timing(scale, {
+          toValue: 1, // 축소 비율
+          duration: 500, // 애니메이션 시간 (밀리초)
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, [scale]);
 
   return (
     <>
-      <View style={styles.container}>
-        <Scan />
-        <TouchableOpacity
-          style={styles.positionButton}
-          onPress={() => {
-            onPosition();
-          }}
-        >
-          <Text style={styles.positionButtonText}>POSITION SAVE</Text>
-        </TouchableOpacity>
-        <ResultModal visible={modalVisible} onClose={closeModal} />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={openModal}>
-        <Text style={styles.buttonText}>LOG</Text>
+      <ImageBackground
+        source={require("./assets/images/map.png")}
+        style={styles.container}
+      >
+        <Animated.View
+          style={[
+            styles.point,
+            {
+              left: position.x - 25,
+              top: position.y - 25,
+              transform: [{ scale }], // 애니메이션으로 크기 조정
+            },
+          ]}
+          {...panResponder.panHandlers}
+        />
+      </ImageBackground>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>SCAN</Text>
       </TouchableOpacity>
     </>
   );
@@ -56,33 +74,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
   },
   button: {
     position: "absolute",
-    top: 10,
-    right: 10,
+    top: 20,
+    right: 20,
     padding: 10,
-    backgroundColor: "#555555",
+    backgroundColor: "#0f0092",
     borderRadius: 5,
   },
   buttonText: {
     color: "white",
     fontWeight: "bold",
   },
-  positionButton: {
-    marginTop: 20,
-    alignSelf: "center",
-    padding: 10,
-    backgroundColor: "#ffd000",
-    borderRadius: 5,
-    width: 150,
-    marginTop: 10,
-  },
-  positionButtonText: {
-    color: "white",
-    fontWeight: "bold",
-    alignSelf: "center",
+  point: {
+    width: 25,
+    height: 25,
+    backgroundColor: "#ffa600ce",
+    borderRadius: 25,
+    position: "absolute",
   },
 });
 
